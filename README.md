6d7ccc44da2903fbf132a29fd3f22b0faf93b63c
Alex C-G <alexcg@outlook.com>
2021-06-09 12:22:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Create venv
+2. `pip install --pre jina[http]`
+3. `jina hello fork chatbot backend`
+4. `pip install transformers torch`
+5. `python app.py` to test
+6. Download [dataset](https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games)
+7. Move `appstore_games.csv` to `./data` 
+8. Edit files to make it work
---
+++ backend/app.py
@@ -0,0 +1,71 @@
+import os
+from pprint import pprint
+import pretty_errors
+from jina.types.arrays.memmap import DocumentArrayMemmap
+from jina import Flow, Document, DocumentArray
+from jina.parsers.helloworld import set_hw_chatbot_parser
+import csv
+
+if __name__ == "__main__":
+    from executors import MyTransformer, MyIndexer
+else:
+    from .executors import MyTransformer, MyIndexer
+
+
+def trim_string(input_string, word_count=50, sep=" "):
+    sanitized_string = input_string.replace("\\n", sep).replace("\\u2022", sep)
+    words = sanitized_string.split(sep)[:word_count]
+    output = " ".join(words)
+
+    return output
+
+
+def prep_docs(input_file, max_docs=1000):
+    # docs = DocumentArrayMemmap("./mem_map")
+    # docs.clear()
+    docs = DocumentArray()
+    with open(input_file, "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        input_field = "Description"
+        for row in csv_reader:
+            input_data = trim_string(row[input_field])
+            doc = Document(text=input_data)
+            doc.tags = row
+            # del doc.tags[input_field]
+            # doc = Document(content="foo")
+            print(doc)
+            docs.extend([doc])
+
+    # docs.prune()
+    return docs
+
+
+def run(inputs, args, top_k=10):
+    """
+    Execute the chatbot example.
+
+    :param args: arguments passed from CLI
+    """
+
+    f = (
+        Flow()
+        .add(uses=MyTransformer, parallel=args.parallel)
+        .add(uses=MyIndexer, workspace=args.workdir)
+    )
+
+    # # index it!
+    with f:
+        f.post(on="/index", inputs=inputs, on_done=print)
+        f.use_rest_gateway(args.port_expose)
+        f.block()
+
+
+if __name__ == "__main__":
+
+    args = set_hw_chatbot_parser().parse_args(); pprint(args)
+
+    docs = prep_docs(input_file="./data/1000.csv")
+    print(docs)
+    print(len(docs))
+
+    run(inputs=docs, args=args)
