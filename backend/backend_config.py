40e24c9893a0d36b6568bb6a9ac6462109835f9d
Alex C-G <alexcg@outlook.com>
2021-06-22 15:43:01 +0200

    fix: trying docarraymemmap to save memory

--- backend/backend_config.py
+++ backend/backend_config.py
@@ -5,4 +5,11 @@ backend_top_k = 10
 # in app.py
 backend_port = 45678
 backend_workdir = "workspace"
-backend_datafile = "./data/1000.csv"
+backend_datafile = "./data/appstore_games-shuffled.csv"
+text_length = 50 # How many words to index for each app? Longer = more accurate, shorter = quicker
+max_docs = 100 # How many apps to index
+random_seed = 42 # Ensure we can replicate shuffling so we get consistent results
+
+# dataset
+dataset_url = "https://github.com/alexcg1/ml-datasets/blob/master/nlp/strategy_games/appstore_games.csv?raw=true" 
+dataset_filename = 'appstore_games.csv'
