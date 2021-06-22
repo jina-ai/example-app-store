f37d54bcd47c8c7cff844ada0b416432a917b1ee
Alex C-G <alexcg@outlook.com>
2021-06-22 15:43:01 +0200

    fix: trying docarraymemmap to save memory

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 import os
+import itertools
 from pprint import pprint
 from jina import Flow, Document, DocumentArray
+from jina.types.arrays.memmap import DocumentArrayMemmap
 from jina.parsers.helloworld import set_hw_chatbot_parser
 import csv
-from backend_config import backend_port, backend_workdir, backend_datafile
+from backend_config import backend_port, backend_workdir, backend_datafile, text_length, max_docs
 from executors import MyTransformer, MyIndexer
 
 try:
@@ -11,7 +13,8 @@ try:
 except ImportError:
     pass
 
-def trim_string(input_string: str, word_count: int = 50, sep: str = " ") -> str:
+
+def trim_string(input_string: str, word_count: int = text_length, sep: str = " ") -> str:
     """
     Trim a string to a certain number of words.
     :param input_string: string to trim
@@ -26,17 +29,19 @@ def trim_string(input_string: str, word_count: int = 50, sep: str = " ") -> str:
     return trimmed_string
 
 
-def prep_docs(input_file: str) -> DocumentArray:
+def prep_docs(input_file: str, max_docs=max_docs):
     """
     Create DocumentArray consisting of every row in csv as a Document
     :param input_file: Input csv filename
     :return: populated DocumentArray
     """
-    docs = DocumentArray()
+    # docs = DocumentArray()
+    docs = DocumentArrayMemmap('./my-memmap')
+    docs.clear() # Clear just in case
     with open(input_file, "r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         input_field = "Description"
-        for row in csv_reader:
+        for row in itertools.islice(csv_reader, max_docs):
             input_data = trim_string(row[input_field])
             doc = Document(text=input_data)
             doc.tags = row
@@ -59,16 +64,19 @@ def run_appstore_flow(inputs, args) -> None:
     flow = (
         Flow()
         .add(uses=MyTransformer, parallel=args.parallel)
+        # .add(uses=EmbeddingIndexer)
+        # .add(uses=KeyValueIndexer)
         .add(uses=MyIndexer, workspace=args.workdir)
     )
+    # flow = Flow.load_config('flows/index.yml')
 
     # Open the Flow
     with flow:
         # Start index pipeline, taking inputs then printing the processed DocumentArray
-        flow.post(on="/index", inputs=inputs, on_done=print)
-        
-        # Start REST gateway so clients can query via Streamlit or other frontend (like Jina Box) 
-        flow.use_rest_gateway(args.port_expose)
+        flow.post(on="/index", inputs=inputs)
+
+        # Start REST gateway so clients can query via Streamlit or other frontend (like Jina Box)
+        flow.use_rest_gateway(backend_port)
 
         # Block the process to keep it open. Otherwise it will just close and no-one could connect
         flow.block()
@@ -80,7 +88,6 @@ if __name__ == "__main__":
     args = set_hw_chatbot_parser().parse_args()
 
     # Change a few things
-    args.port_expose = backend_port
     args.workdir = backend_workdir
 
     # Convert the csv file to a DocumentArray
