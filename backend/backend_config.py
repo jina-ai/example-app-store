a5d89dc0e301265af467262b12cd04f14782dfa9
Alex C-G <alexcg@outlook.com>
2021-06-23 09:33:47 +0200

    chore: reorder config

--- backend/backend_config.py
+++ backend/backend_config.py
@@ -7,9 +7,9 @@ backend_port = 45678
 backend_workdir = "workspace"
 backend_datafile = "./data/appstore_games-shuffled.csv"
 text_length = 50 # How many words to index for each app? Longer = more accurate, shorter = quicker
-max_docs = 100 # How many apps to index
-random_seed = 42 # Ensure we can replicate shuffling so we get consistent results
+max_docs = 5000 # How many apps to index
 
 # dataset
 dataset_url = "https://github.com/alexcg1/ml-datasets/blob/master/nlp/strategy_games/appstore_games.csv?raw=true" 
 dataset_filename = 'appstore_games.csv'
+random_seed = 42 # Ensure we can replicate shuffling so we get consistent results
