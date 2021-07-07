e19ae5df6ea846b09b87be94a766770daacc7751
Alex C-G <alexcg@outlook.com>
2021-07-07 09:52:23 +0200

    fix(get_data): s3 url

--- backend_config.py
+++ backend_config.py
@@ -10,6 +10,6 @@ text_length = 50 # How many words to index for each app? Longer = more accurate,
 max_docs = 3000 # How many apps to index. Can override with "-n" flag
 
 # dataset
-dataset_url = "https://github.com/alexcg1/ml-datasets/blob/master/nlp/strategy_games/appstore_games.csv?raw=true" # URL to download dataset
+dataset_url = "https://jina-examples-datasets.s3.amazonaws.com/app-store/appstore_games.csv" # URL to download dataset
 dataset_filename = 'appstore_games.csv' # Original dataset filename
 random_seed = 42 # Ensure we can replicate shuffling so we get consistent results
