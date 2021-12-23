452e945e962326dc614bb7b2525e649be3f5c0ff
Alex C-G <alexcg@outlook.com>
2021-12-23 15:04:44 +0000

    feat: docker-compose

--- frontend/config.py
+++ frontend/config.py
@@ -1,5 +1,7 @@
-SERVER = "0.0.0.0"
+import os
+
+SERVER = os.getenv("BACKEND_SERVER", "0.0.0.0")
 PORT = 45678
-endpoint = "http://0.0.0.0:45678/search"
-top_k = 10
+# endpoint = "http://0.0.0.0:45678/search"
+# top_k = 10
 TOP_K = 10
